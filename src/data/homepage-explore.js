40e6ae4e21d2d9f56daa0c78fb9aaeee1cde5fda
codeXaditya <adityabarotha007@gmail.com>
2024-09-15 20:05:01 +0530

    first commit

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Learn HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive Web design",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive ",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Java",
+                description : "This course introduces fundamental Java concepts, covering syntax, data types, control structures, object-oriented principles, and application development",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Python",
+                description : "This program teaches fundamental Python concepts, enabling coding proficiency in data manipulation, automation, algorithms, and software development",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "SCSS",
+                description : "This course introduces SCSS, teaching nested styles, variables, mixins, and efficient CSS compilation for enhanced web design",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Flask",
+                description : "Flask teaches Python web app development, offering simplicity to create dynamic sites with routing, templates, forms, and extensions.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Django",
+                description : "Django course teaches creating web apps with Python, covering models, views, templates, authentication, and database integration for modern development",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Fast API",
+                description : "FastAPI course covers creating APIs in Python with high performance, automatic validation, and interactive documentation for modern web applications",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Next.js",
+                description : "Next.js teaches web development with React, providing server-rendered, SEO-friendly, and efficient client-side routing for modern web applications",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Nuxt.js",
+                description : "Nuxt.js course: Learn to build modern web applications with Vue.js, server-side rendering, routing, and state management for enhanced user experiences",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Sanity",
+                description : "Sanity course covers essential HTML concepts: page structure, text, links, images, providing foundational knowledge for web development",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+]
